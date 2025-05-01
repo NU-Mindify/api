6dfbde60df7b6c6c923b18a27b631c680bcfb003
jvimperial <joshuaimperial1010@gmail.com>
2025-05-02 06:24:28 +0800

    Moved analytics to its own file, removed limit (30) in analytics controller

--- src/controllers/attempts.controller.js
+++ src/controllers/attempts.controller.js
@@ -86,39 +86,4 @@ async function getTopLeaderboards(req, res) {
   }
 }
 
-async function getAnalytics(req, res) {
-  try {
-    const { categories, levels, mode } = req.query;
-
-    if (!categories || !levels) {
-      throw new Error("Categories or levels params not found!");
-    }
-
-    // Convert categories and levels into arrays
-    const categoryArray = categories.split(',');
-    const levelArray = levels.split(',');
-    const modeArray = mode.split(',');
-  
-    const queries = {
-      category: { $in: categoryArray }, // Match any of the categories in the list
-      level: { $in: levelArray },       // Match any of the levels in the list
-      mode: { $in: modeArray }          // Match any of the modes in the list
-    };
-
-    const leaderboardData = await AttemptsModel.find(queries)
-      .populate('user_id')
-      .limit(30)
-      .sort({
-        correct: 'desc',
-        time_completion: 'asc',
-        created_at: 'desc'
-      });
-
-    res.json(leaderboardData);
-  } catch (error) {
-    res.status(422).json({ error: error.message });
-  }
-}
-
-
-module.exports = { getLeaderboard, addAttempt, getTopLeaderboards, getAnalytics}
+module.exports = { getLeaderboard, addAttempt, getTopLeaderboards}
