49c38d01a814ffa43f591d4d92d512ea703f9a84
Francis Salles <fjsalles.04@gmail.com>
2025-06-10 05:37:40 +0800

    init sanitization

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./src/routes/routes');
 const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 8080
 const uri = process.env.MONGO;
@@ -12,6 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(mongoSanitize({
+  allowDots: true,
+  replaceWith: '_',
+}),);
 
 const clientOptions = {
   serverApi: {
