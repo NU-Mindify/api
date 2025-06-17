b328a120e3281f92255adf5cfc16203c5c435a4b
gialim <gianette0808@gmail.com>
2025-06-17 16:48:47 +0800

    display badges
    get top earned badge; display percentage of students that earned the badge

--- src/controllers/badges.controller.js
+++ src/controllers/badges.controller.js
@@ -1,5 +1,6 @@
 const Badges = require('../models/Badges')
 const UserBadges = require('../models/UserBadges')
+const Users = require('../models/Users');
 
 async function addBadge(req, res) {
   try {
@@ -47,4 +48,43 @@ async function getAllBadges(req, res) {
   
 }
 
-module.exports = { addBadge, addUserBadge, getUserBadges, getAllBadges }
+async function getTopEarnedBadges(req, res) {
+  try {
+    const totalUsers = await Users.countDocuments({ is_deleted: false });
+
+    const topBadges = await UserBadges.aggregate([
+      { $group: { _id: "$badge_id", count: { $sum: 1 }, users: { $addToSet: "$user_id" } } },
+      { $sort: { count: -1 } },
+      { $limit: 10 },
+      {
+        $lookup: {
+          from: "badges",
+          localField: "_id",
+          foreignField: "_id",
+          as: "badge"
+        }
+      },
+      { $unwind: "$badge" }
+    ]);
+
+   
+    const formatted = topBadges.map(b => {
+      const usersWithBadge = b.users.length;
+      const percentage = totalUsers > 0 ? ((usersWithBadge / totalUsers) * 100).toFixed(2) : "0.00";
+      return {
+        id: b._id,
+        count: b.count,
+        name: b.badge.category + " Lv." + b.badge.level,
+        iconUrl: b.badge.filepath,
+        percentage: Number(percentage)
+      };
+    });
+
+    res.json(formatted);
+  } catch (error) {
+    console.error("Error in getTopEarnedBadges:", error);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { addBadge, addUserBadge, getUserBadges, getAllBadges, getTopEarnedBadges }
