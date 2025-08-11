a94df161e34cd53df21bf6584d13fa0b7a64f8b4
Francis Salles <fjsalles.04@gmail.com>
2025-08-11 11:10:23 +0800

    Updated AI to gemini 2.5 for mindmap

--- src/controllers/mindmap.controller.js
+++ src/controllers/mindmap.controller.js
@@ -22,7 +22,7 @@ async function generateMindmap(req, res) {
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-8b", // or gemini-1.5-flash, as they support structured output
+  model: "gemini-2.5-flash",
   generationConfig: {
     responseMimeType: "application/json",
     responseSchema: {
@@ -73,16 +73,14 @@ const model = genAI.getGenerativeModel({
 });
 
 async function generateJSON(userQuery) {
-  const prompt = `You are an AI assistant for generating mind maps. A user has provided a topic. Your task is to generate a mind map with a central topic and several sub-topics with short description. For each sub-topic, provide 2-3 related ideas. The mind map should be structured in a way that is logical for visualization.
+  const prompt = `You are an AI assistant for generating mind maps. A user has provided a topic. Your task is to generate a mind map with a central topic and several sub-topics with description. For each sub-topic, provide 2 or more related ideas depending on how broad the topic is. The mind map should be structured in a way that is logical for visualization.
 
   User Topic: "${userQuery}"
 
-  Remember to fill in the 'label' for each node and to connect the nodes with edges. The central topic should have an id of '1'. The position of the nodes should be arranged in a clear layout, such as a radial structure. The value of x and y is in pixels. There should not be a node with same position.`;
+  Remember to fill in the 'label' for each node and to connect the nodes with edges. The central topic should have an id of '1'. The position of the nodes should be arranged in a clear layout, such as a tree or radial structure. The value of x and y is in pixels and should be unique and far enough to not overlap with other nodes.`;
 
   const result = await model.generateContent(prompt);
 
-  // The API response will already be a valid JSON object thanks to the schema.
-  // You can parse it directly.
   const response = result.response.text();
   const mindMapData = JSON.parse(response);
 
