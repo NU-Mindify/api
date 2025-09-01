3f07fa13021aa85a99299a207438c3bac5b16d58
Morej <jeromeaw02@gmail.com>
2025-09-01 21:28:43 +0800

    mali pala, update nga lang pala yung is_deleted hehe

--- src/controllers/web/branches.controller.js
+++ src/controllers/web/branches.controller.js
@@ -32,18 +32,19 @@ const addBranches = (req, res) => {
 
 const deleteBranch = async (req, res) => {
   try {
-    const { id } = req.params;
+    const {branchId, is_deleted} = req.body;
 
-    const deletedBranch = await BranchesModel.findByIdAndDelete(id);
+    const deletedBranch = await BranchesModel.findByIdAndUpdate(
+        branchId, 
+        { is_deleted }, 
+        { new: true }
+    );
 
     if (!deletedBranch) {
       return res.status(404).json({ message: "Branch not found" });
     }
 
-    res.status(200).json({ 
-      message: "Branch deleted successfully", 
-      deletedBranch 
-    });
+    res.json(deletedBranch);
   } catch (error) {
     console.error("Error deleting branch:", error);
     res.status(500).json({ message: "Internal server error", error: error.message });
