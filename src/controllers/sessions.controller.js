f0372f267a3b372333d5b1dce80919e28cdf2375
gialim <gianette0808@gmail.com>
2025-09-02 20:01:40 +0800

    ave session time in Show More Details
    add getAverageSessionTime in controller and route

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -16,4 +16,19 @@ async function addSession(req, res) {
   }
 }
 
-module.exports = {addSession}
+async function getAverageSessionTime(req, res) {
+  try {
+    const result = await SessionsModel.aggregate([
+      { $match: { duration: { $exists: true, $ne: null } } },
+      { $group: { _id: null, avgDuration: { $avg: "$duration" } } }
+    ]);
+    const avgDuration = result.length > 0 ? result[0].avgDuration : 0;
+    res.json({ averageSessionTime: avgDuration });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+}
+rs
+
+module.exports = {addSession, getAverageSessionTime}
